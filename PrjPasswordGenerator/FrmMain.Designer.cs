bf8121d1ce89b4a373a75003faa8987c6eebc04f
Adonis Mendoza Perez <adonis.mendoza@outlook.com>
2017-03-24 15:31:38 -0400

    primer commit

---
+++ PrjPasswordGenerator/FrmMain.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace PrjPasswordGenerator
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtLength = new System.Windows.Forms.NumericUpDown();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.chkNumbers = new MetroFramework.Controls.MetroCheckBox();
+            this.chkLetters = new MetroFramework.Controls.MetroCheckBox();
+            this.btnClear = new MetroFramework.Controls.MetroButton();
+            this.btnCopy = new MetroFramework.Controls.MetroButton();
+            this.txtPassword = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.btnGenerate = new MetroFramework.Controls.MetroButton();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtLength)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtLength);
+            this.groupBox1.Controls.Add(this.metroLabel1);
+            this.groupBox1.Controls.Add(this.chkNumbers);
+            this.groupBox1.Controls.Add(this.chkLetters);
+            this.groupBox1.Location = new System.Drawing.Point(43, 94);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(366, 72);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Settings";
+            // 
+            // txtLength
+            // 
+            this.txtLength.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtLength.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLength.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.txtLength.Location = new System.Drawing.Point(125, 31);
+            this.txtLength.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.txtLength.Minimum = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(54, 18);
+            this.txtLength.TabIndex = 5;
+            this.txtLength.Value = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(11, 28);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(109, 19);
+            this.metroLabel1.TabIndex = 4;
+            this.metroLabel1.Text = "Password Length:";
+            // 
+            // chkNumbers
+            // 
+            this.chkNumbers.AutoSize = true;
+            this.chkNumbers.Location = new System.Drawing.Point(249, 34);
+            this.chkNumbers.Name = "chkNumbers";
+            this.chkNumbers.Size = new System.Drawing.Size(72, 15);
+            this.chkNumbers.TabIndex = 2;
+            this.chkNumbers.Text = "Numbers";
+            this.chkNumbers.UseSelectable = true;
+            // 
+            // chkLetters
+            // 
+            this.chkLetters.AutoSize = true;
+            this.chkLetters.Location = new System.Drawing.Point(185, 34);
+            this.chkLetters.Name = "chkLetters";
+            this.chkLetters.Size = new System.Drawing.Size(58, 15);
+            this.chkLetters.TabIndex = 1;
+            this.chkLetters.Text = "Letters";
+            this.chkLetters.UseSelectable = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(332, 222);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(77, 23);
+            this.btnClear.Style = MetroFramework.MetroColorStyle.Teal;
+            this.btnClear.TabIndex = 14;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseSelectable = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(262, 222);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(64, 23);
+            this.btnCopy.Style = MetroFramework.MetroColorStyle.Teal;
+            this.btnCopy.TabIndex = 13;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseSelectable = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // txtPassword
+            // 
+            // 
+            // 
+            // 
+            this.txtPassword.CustomButton.Image = null;
+            this.txtPassword.CustomButton.Location = new System.Drawing.Point(206, 1);
+            this.txtPassword.CustomButton.Name = "";
+            this.txtPassword.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.txtPassword.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtPassword.CustomButton.TabIndex = 1;
+            this.txtPassword.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtPassword.CustomButton.UseSelectable = true;
+            this.txtPassword.CustomButton.Visible = false;
+            this.txtPassword.Lines = new string[0];
+            this.txtPassword.Location = new System.Drawing.Point(181, 181);
+            this.txtPassword.MaxLength = 20;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '\0';
+            this.txtPassword.PromptText = "Password";
+            this.txtPassword.ReadOnly = true;
+            this.txtPassword.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtPassword.SelectedText = "";
+            this.txtPassword.SelectionLength = 0;
+            this.txtPassword.SelectionStart = 0;
+            this.txtPassword.ShortcutsEnabled = true;
+            this.txtPassword.Size = new System.Drawing.Size(228, 23);
+            this.txtPassword.TabIndex = 12;
+            this.txtPassword.UseSelectable = true;
+            this.txtPassword.WaterMark = "Password";
+            this.txtPassword.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtPassword.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(43, 181);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(132, 19);
+            this.metroLabel2.TabIndex = 11;
+            this.metroLabel2.Text = "Generated password:";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnGenerate.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnGenerate.Location = new System.Drawing.Point(181, 222);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.Style = MetroFramework.MetroColorStyle.Teal;
+            this.btnGenerate.TabIndex = 10;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.btnGenerate.UseSelectable = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 319);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.btnGenerate);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(485, 319);
+            this.MinimumSize = new System.Drawing.Size(485, 319);
+            this.Name = "FrmMain";
+            this.ShowIcon = false;
+            this.Text = "Password Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtLength)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown txtLength;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroCheckBox chkNumbers;
+        private MetroFramework.Controls.MetroCheckBox chkLetters;
+        private MetroFramework.Controls.MetroButton btnClear;
+        private MetroFramework.Controls.MetroButton btnCopy;
+        private MetroFramework.Controls.MetroTextBox txtPassword;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroButton btnGenerate;
+    }
+}
+
